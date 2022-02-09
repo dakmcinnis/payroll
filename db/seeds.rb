8e3c45c63240f5ead67fe9994bbd35cb09cb4962
Dakota McInnis <dakotamcinnis1@gmail.com>
2022-02-09 01:55:33 -0500

    Seed: Add pay periods and intervals

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
+# Users
+
 admin = User.new
 admin.admin = true
 admin.first_name = "Dakota"
@@ -29,3 +31,55 @@ employee2.pronouns = "he/him"
 employee2.password = ENV["SEED_PASSWORD"]
 employee2.password_confirmation = ENV["SEED_PASSWORD"]
 employee2.save!
+
+# Pay Periods
+
+PayPeriod.create(
+    active: false,
+    start: (Time.now - 4.weeks).to_date,
+    end: (Time.now - 2.weeks - 1.day).to_date,
+    call: (Time.now - 2.weeks).to_date
+)
+
+PayPeriod.create(
+    active: false,
+    start: (Time.now - 2.weeks).to_date,
+    end: (Time.now - 1.day).to_date,
+    call: (Time.now).to_date
+)
+
+PayPeriod.create(
+    active: true,
+    start: (Time.now).to_date,
+    end: (Time.now + 2.weeks - 1.day).to_date,
+    call: (Time.now + 2.weeks).to_date
+)
+
+# Time Intervals
+
+users = User.all
+pay_periods = PayPeriod.all
+
+users.each do |user|
+    pay_periods.each do |pay_period|
+        i = 0
+        for i in (0..13)
+            date = pay_period.start + i.days
+            next if date.saturday? || date.sunday?
+            Interval.create(
+                user: user.id,
+                pay_period: pay_period.id,
+                date: date,
+                time_in: date + 8.hours,
+                time_out: date + 12.hours
+            )
+            Interval.create(
+                user: user.id,
+                pay_period: pay_period.id,
+                date: date,
+                time_in: date + 13.hours,
+                time_out: date + 17.hours
+            )
+        end
+    end
+end
