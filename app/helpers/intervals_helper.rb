d1eec7901c85b247fadd6519c2699f0480cca385
Dakota McInnis <dakotamcinnis1@gmail.com>
2022-02-09 01:44:43 -0500

    Intervals: format times to not display date

--- app/helpers/intervals_helper.rb
+++ app/helpers/intervals_helper.rb
@@ -1,2 +1,5 @@
 module IntervalsHelper
+    def format_time(time)
+        time.strftime("%l:%M %p").strip
+    end
 end
---
+++ test/helpers/intervals_helper_test.rb
@@ -0,0 +1,34 @@
+require "test_helper"
+
+class IntervalsHelperTest < ActionView::TestCase
+
+  # For format_time
+
+  test "format_time at midnight" do
+    expected = "12:00 AM"
+    result = format_time(Time.new(2011, 10, 31))
+
+    assert_equal expected, result
+  end
+
+  test "format_time at 3:45am" do
+    expected = "3:45 AM"
+    result = format_time(Time.new(2011, 10, 31, 3, 45, 01))
+
+    assert_equal expected, result
+  end
+
+  test "format_time at noon" do
+    expected = "12:00 PM"
+    result = format_time(Time.new(2011, 10, 31, 12))
+
+    assert_equal expected, result
+  end
+
+  test "format_time at 11:59:59pm" do
+    expected = "11:59 PM"
+    result = format_time(Time.new(2011, 10, 31, 23, 59, 59))
+
+    assert_equal expected, result
+  end
+end
