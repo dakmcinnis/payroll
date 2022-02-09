f7c13b3e509ab2aa6955a6970ec1465e3a3ab738
Dakota McInnis <dakotamcinnis1@gmail.com>
2022-02-08 20:30:22 -0500

    Add viewing users page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :intervals
   devise_for :users
+  match '/users', to: 'users#index', via: 'get'
+  match '/users/:id', to: 'users#show', via: 'get'
+  resources :users, :only =>[:show]
   resources :pay_periods
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
