6ff7acc149ff672186a40a10de5832648eb5ae53
Dakota McInnis <dakotamcinnis1@gmail.com>
2022-02-08 22:24:41 -0500

    User: Add admin field and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_09_013952) do
+ActiveRecord::Schema.define(version: 2022_02_09_025828) do
 
   create_table "intervals", force: :cascade do |t|
     t.integer "user"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2022_02_09_013952) do
     t.datetime "remember_created_at", precision: 6
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+admin = User.new
+admin.admin = true
+admin.email = ENV["SEED_ADMIN_EMAIL"]
+admin.password = ENV["SEED_PASSWORD"]
+admin.password_confirmation = ENV["SEED_PASSWORD"]
+admin.save!
+
+employee1 = User.new
+employee1.email = "tammy@gmail.com"
+employee1.password = ENV["SEED_PASSWORD"]
+employee1.password_confirmation = ENV["SEED_PASSWORD"]
+employee1.save!
+
+employee2 = User.new
+employee2.email = "joseph@gmail.com"
+employee2.password = ENV["SEED_PASSWORD"]
+employee2.password_confirmation = ENV["SEED_PASSWORD"]
+employee2.save!
+
