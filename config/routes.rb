39982006f55f6ea49f529a2982de145abd78ba83
Dakota McInnis <dakotamcinnis1@gmail.com>
2022-02-10 21:45:09 -0500

    Remove unnnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :users
   get '/latest_timesheet', to: 'timesheets#show_latest'
   resources :timesheets do
     resources :intervals
   end
-  resources :intervals
-  devise_for :users
   authenticate :user, ->(user) { user.admin? } do
     resources :admin, only: :index do
       collection do
         resources :users
-        resources :pay_periods do
-          resources :timesheets do
-            resources :intervals
-          end
-        end
+        resources :pay_periods
+        # TODO: Add timesheets and intervals beneath payperiods
+        # Admins should be able to view & edit all timesheets
       end
     end
   end
-  resources :pay_periods
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   root to: redirect('/latest_timesheet')
 end
